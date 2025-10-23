066fe9739e90b114c760c04fb54adae3fd16393a
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-10-23 21:29:02 +0300

    feat: adjust th for table , add cart to routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/Landing/Landing.tsx";
 import Login from "./pages/Login/Login.tsx";
+import Cart from "./pages/Cart/Cart.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Landing /> },
       { path: "login", element: <Login /> },
+      { path: "/cart", element: <Cart /> },
     ],
   },
 ]);
