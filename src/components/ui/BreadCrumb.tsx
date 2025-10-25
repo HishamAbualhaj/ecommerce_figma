64ca66545d06a012e67f16e21ab7bf509c84d0db
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-10-25 22:46:04 +0200

    fix: adjust breadcrumb to handle dynamic segments

--- src/components/ui/BreadCrumb.tsx
+++ src/components/ui/BreadCrumb.tsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 
 interface BreadCrumbProps {
   label: string;
+  segment?: string;
 }
-const BreadCrumb = ({ label }: BreadCrumbProps) => {
+const BreadCrumb = ({ label, segment }: BreadCrumbProps) => {
   const breadcrumbMap: Record<string, { label: string; href?: string }[]> = {
     "/cart": [{ label: "Home", href: "/" }, { label: "Cart" }],
     "/checkout": [
@@ -13,11 +14,16 @@ const BreadCrumb = ({ label }: BreadCrumbProps) => {
       { label: "View Cart", href: "/cart" },
       { label: "CheckOut" },
     ],
+    "/productDetails": [
+      { label: "Account", href: "/" },
+      { label: "Gaming", href: "/" },
+      { label: segment ?? "" },
+    ],
   };
   return (
     <>
-      <div className="flex gap-3">
-        {breadcrumbMap[label].map((data, i) => (
+      <div className="flex flex-wrap gap-3">
+        {breadcrumbMap?.[label]?.map((data, i) => (
           <div key={i} className="flex gap-3">
             {data.href ? (
               <Link className="text-gray-400" to={data?.href}>
