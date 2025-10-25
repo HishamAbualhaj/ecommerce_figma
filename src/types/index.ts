c4099b117e2439c3218575ca6c9c18c6a0e24eee
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-10-25 22:49:05 +0200

    feat: Create type file to handle type safey

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type ProductCart = {
+  productId: string;
+  productName: string;
+  product: React.ReactNode;
+  price: React.ReactNode;
+  quantity: React.ReactNode;
+  total: React.ReactNode;
+};
